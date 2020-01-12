5a5deda522b1f99141dc55f0c7324d99a9c180e9
wildcat <letranhaitung@gmail.com>
2020-01-13 03:00:32 +0700

    oops forgot the port

--- service-a/main.go
+++ service-a/main.go
@@ -71,5 +71,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe("0.0.0.0:8081", nil))
+	log.Fatal(http.ListenAndServe("0.0.0.0:80", nil))
 }
