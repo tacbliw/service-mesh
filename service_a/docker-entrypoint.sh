db689b67bb919842b596d0e7558211f4463004ea
wildcat <letranhaitung@gmail.com>
2020-01-13 00:48:47 +0700

    sleepyyy

--- service_a/docker-entrypoint.sh
+++ service_a/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sleep 4
 consul agent -config-dir /consul/config &
 consul connect envoy -sidecar-for service_a > /envoy.out &
 go run main.go
--- service_c/docker-entrypoint.sh
+++ service_c/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sleep 4
 consul agent -config-dir /consul/config &
 consul connect envoy -sidecar-for service_c > /envoy.out &
 go run main.go
