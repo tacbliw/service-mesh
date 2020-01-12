0f34dd4dc5ec820ca64f49a650099a27967ddea1
wildcat <letranhaitung@gmail.com>
2020-01-13 01:34:39 +0700

    verbose

--- service_b/main.go
+++ service_b/main.go
@@ -8,11 +8,11 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-
 	fmt.Fprintf(w, "Hello from service B  ")
 }
 
 func main() {
 	http.HandleFunc("/", handler)
+	fmt.Println("I'm running...")
 	log.Fatal(http.ListenAndServe(":8788", nil))
 }
